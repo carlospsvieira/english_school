815f625c935ca00b670e4b3e55fa8433f968204f
Carlos Vieira <carlepsvieira@gmail.com>
2023-01-17 11:28:25 -0300

    testimonials done and styled nav opacity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../stylesheets/Hero.css";
 
 export default function Hero() {
+  const handleClickScroll = () => {
+    const element = document.querySelector(".programs");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="hero-container">
       <div className="heading">
         <h2>It's a fun old world</h2>
         <h1>Where the new is always present.</h1>
-        <Link to="/pricing">CHECK OUT WHEN OUR CLASSES START</Link>
+        <button type="button" onClick={handleClickScroll}>
+          CHECK OUT OUR PROGRAMS
+        </button>
       </div>
     </div>
   );
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
+import "../stylesheets/Testimonials.css";
+import student1 from "../images/student1.jpg";
+import student2 from "../images/student2.jpg";
+import student3 from "../images/student3.jpg";
+import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 
 export default function Testimonials() {
   return (
-    <div>Testimonials</div>
-  )
+    <>
+      <div className="test-container">
+        <h1>WHAT OUR STUDENTS HAVE TO SAY</h1>
+        <div className="card-container">
+          <img src={student1} alt="student_1" />
+          <div className="text-container">
+            <h3>Clara Silva</h3>
+            <span>
+              <RiDoubleQuotesL />
+              Turpis egestas pretium aenean pharetra magna ac placerat. Donec
+              adipiscing tristique risus nec. Faucibus a pellentesque sit amet
+              porttitor eget dolor morbi.
+              <RiDoubleQuotesR />
+            </span>
+          </div>
+        </div>
+        <div className="card-container">
+          <img src={student2} alt="student_2" />
+          <div className="text-container">
+            <h3>Igor Shevchenko</h3>
+            <span>
+              <RiDoubleQuotesL />
+              Amet consectetur adipiscing elit ut aliquam purus. Velit laoreet
+              id donec ultrices tincidunt arcu non. Porta non pulvinar neque
+              laoreet suspendisse.
+              <RiDoubleQuotesR />
+            </span>
+          </div>
+        </div>
+        <div className="card-container">
+          <img src={student3} alt="student_2" />
+          <div className="text-container">
+            <h3>Pierre Concordia</h3>
+            <span>
+              <RiDoubleQuotesL />
+              Amet consectetur adipiscing elit ut aliquam purus. Velit laoreet
+              id donec ultrices tincidunt arcu non. Porta non pulvinar neque
+              laoreet suspendisse.
+              <RiDoubleQuotesR />
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
