4f52e32844fe5b67c43805bea70ada2440e2de97
Carlos Vieira <carlepsvieira@gmail.com>
2023-01-17 20:04:41 -0300

    dates and pricing done

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/Pricing.css";
 
 export default function Pricing() {
   return (
-    <div>Pricing</div>
-  )
+    <div className="pricing-container">
+      <h1>Dates & Pricing</h1>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Program</th>
+            <th scope="col">Start/End Date</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>On Campus</td>
+            <td>Feb 5 - May 5</td>
+            <td>$3000</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Online</td>
+            <td>Anytime</td>
+            <td>$25/class</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Summer Classes</td>
+            <td>May 31 - Sep 16</td>
+            <td>$4500</td>
+          </tr>
+        </tbody>
+      </table>
+      <Link to="/register">Register Now!</Link>
+    </div>
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,33 +7,33 @@ export default function Register() {
     <div className="register-container">
       <h1>Register</h1>
       <img src={registration} alt="register" />
-      <form class="row row-cols-lg-auto g-3 align-items-center" action="/">
-        <div class="col-12">
-          <div class="input-group">
+      <form className="row row-cols-lg-auto g-3 align-items-center" action="/">
+        <div className="col-12">
+          <div className="input-group">
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               placeholder="Full Name"
               required
             />
           </div>
         </div>
-        <div class="col-12">
-          <div class="input-group">
+        <div className="col-12">
+          <div className="input-group">
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               placeholder="Email"
               required
             />
           </div>
         </div>
 
-        <div class="col-12">
-          <label class="visually-hidden" for="inlineFormSelectPref">
+        <div className="col-12">
+          <label className="visually-hidden" for="inlineFormSelectPref">
             Preference
           </label>
-          <select class="form-select" id="inlineFormSelectPref" required>
+          <select className="form-select" id="inlineFormSelectPref" required>
             <option selected>Choose your program</option>
             <option value="1">On Campus</option>
             <option value="2">Online</option>
@@ -41,24 +41,22 @@ export default function Register() {
           </select>
         </div>
 
-        <div class="col-12">
-          <div class="form-check">
+        <div className="col-12">
+          <div className="form-check">
             <input
-              class="form-check-input"
+              className="form-check-input"
               type="checkbox"
               id="inlineFormCheck"
               required
             />
-            <label class="form-check-label" for="inlineFormCheck">
-              Agree to send my information  
+            <label className="form-check-label" for="inlineFormCheck">
+              Agree to send my information
             </label>
           </div>
         </div>
 
-        <div class="col-12">
-          <button type="submit">
-            Submit
-          </button>
+        <div className="col-12">
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
