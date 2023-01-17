43310fd99c50fb7079ad1666eb346239fa45528d
Carlos Vieira <carlepsvieira@gmail.com>
2023-01-17 11:58:24 -0300

    about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React from "react";
+import "../stylesheets/About.css";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <div className="about-container">
+        <div className="about-text">
+          <h1>About English School</h1>
+          <span>
+            Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae.
+            Pulvinar mattis nunc sed blandit. Quis viverra nibh cras pulvinar.
+            Tempus imperdiet nulla malesuada pellentesque. Eu volutpat odio
+            facilisis mauris sit amet. Rhoncus aenean vel elit scelerisque
+            mauris pellentesque pulvinar pellentesque. Viverra vitae congue eu
+            consequat ac felis donec. Mi tempus imperdiet nulla malesuada
+            pellentesque. Volutpat maecenas volutpat blandit aliquam etiam.
+            Massa eget egestas purus viverra accumsan in nisl nisi. Lorem ipsum
+            dolor sit amet consectetur adipiscing elit ut. Nisi est sit amet
+            facilisis. Vulputate sapien nec sagittis aliquam malesuada bibendum.
+            Purus ut faucibus pulvinar elementum integer. Etiam non quam lacus
+            suspendisse faucibus interdum posuere lorem ipsum. Varius morbi enim
+            nunc faucibus a pellentesque sit amet. Eleifend mi in nulla posuere
+            sollicitudin aliquam ultrices. Dictum sit amet justo donec enim diam
+            vulputate ut.
+            <br />
+            <br />
+            Purus gravida quis blandit turpis. Ut venenatis tellus in metus
+            vulputate eu scelerisque felis. Egestas diam in arcu cursus euismod
+            quis viverra nibh cras. Risus viverra adipiscing at in tellus
+            integer feugiat. Suspendisse ultrices gravida dictum fusce. Iaculis
+            at erat pellentesque adipiscing commodo elit at. Integer quis auctor
+            elit sed vulputate mi sit amet. Sed velit dignissim sodales ut eu
+            sem integer vitae justo. Laoreet sit amet cursus sit amet dictum
+            sit. Morbi tincidunt augue interdum velit euismod in pellentesque
+            massa. Posuere urna nec tincidunt praesent semper feugiat nibh.
+            Mauris commodo quis imperdiet massa tincidunt nunc. Suscipit
+            adipiscing bibendum est ultricies integer quis. Semper feugiat nibh
+            sed pulvinar proin. Quam elementum pulvinar etiam non quam lacus.
+            Dignissim diam quis enim lobortis scelerisque fermentum. Hendrerit
+            gravida rutrum quisque non tellus orci. Quis imperdiet massa
+            tincidunt nunc pulvinar sapien et.
+            <br />
+            <br />
+            Nam at lectus urna duis convallis convallis tellus. Suspendisse
+            ultrices gravida dictum fusce ut placerat orci nulla. Placerat orci
+            nulla pellentesque dignissim enim sit amet venenatis urna. Aenean et
+            tortor at risus viverra adipiscing at. Euismod elementum nisi quis
+            eleifend quam adipiscing vitae proin. Convallis a cras semper auctor
+            neque. Nulla porttitor massa id neque. Nullam ac tortor vitae purus
+            faucibus ornare suspendisse sed. Faucibus turpis in eu mi bibendum
+            neque. Quam elementum pulvinar etiam non. Pellentesque diam volutpat
+            commodo sed egestas. Morbi tempus iaculis urna id volutpat. Morbi
+            tincidunt ornare massa eget egestas purus. Varius quam quisque id
+            diam vel quam elementum. A iaculis at erat pellentesque adipiscing
+            commodo. Sed enim ut sem viverra aliquet eget sit amet tellus. Fames
+            ac turpis egestas sed tempus.
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
