c2eec79d9fd95391d9dba177854ba43487f478d7
Carlos Vieira <carlepsvieira@gmail.com>
2023-01-17 19:33:41 -0300

    register form done

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,57 +1,57 @@
-import React from 'react'
-import '../stylesheets/Programs.css'
-import onCampus from '../images/on-campus.jpg'
-import onlineClasses from '../images/online-classes.jpg'
-import summerClasses from '../images/summer-classes.jpg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../stylesheets/Programs.css";
+import onCampus from "../images/on-campus.jpg";
+import onlineClasses from "../images/online-classes.jpg";
+import summerClasses from "../images/summer-classes.jpg";
+import { Link } from "react-router-dom";
 
 export default function Programs() {
   return (
-    <div className='programs'>
+    <div className="programs">
       <h1>PROGRAMS</h1>
-      <div className='programs-container'>
-        <div className='program-content'>
+      <div className="programs-container">
+        <div className="program-content">
           <img src={onCampus} alt="on-campus" />
           <h2>ON CAMPUS</h2>
-          <div className='program-text'>
+          <div className="program-text">
             <Link to="/register">
               <span>
-                Amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus.
-                Cras semper auctor neque vitae tempus.
-                Non consectetur a erat nam at lectus urna duis convallis.
-                Non curabitur gravida arcu ac. Accumsan in nisl nisi scelerisque eu ultrices vitae.
+                Amet volutpat consequat mauris nunc congue nisi vitae suscipit
+                tellus. Cras semper auctor neque vitae tempus. Non consectetur a
+                erat nam at lectus urna duis convallis. Non curabitur gravida
+                arcu ac. Accumsan in nisl nisi scelerisque eu ultrices vitae.
                 Gravida arcu ac tortor dignissim convallis aenean et tortor at.
               </span>
               <p>Register Now!</p>
             </Link>
           </div>
         </div>
-        <div className='program-content'>
+        <div className="program-content">
           <img src={onlineClasses} alt="online-classes" />
           <h2>ONLINE</h2>
-          <div className='program-text'>
+          <div className="program-text">
             <Link to="/register">
               <span>
-                Amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus.
-                Cras semper auctor neque vitae tempus.
-                Non consectetur a erat nam at lectus urna duis convallis.
-                Non curabitur gravida arcu ac. Accumsan in nisl nisi scelerisque eu ultrices vitae.
+                Amet volutpat consequat mauris nunc congue nisi vitae suscipit
+                tellus. Cras semper auctor neque vitae tempus. Non consectetur a
+                erat nam at lectus urna duis convallis. Non curabitur gravida
+                arcu ac. Accumsan in nisl nisi scelerisque eu ultrices vitae.
                 Gravida arcu ac tortor dignissim convallis aenean et tortor at.
               </span>
               <p>Register Now!</p>
             </Link>
           </div>
         </div>
-        <div className='program-content'>
+        <div className="program-content">
           <img src={summerClasses} alt="summer-classes" />
           <h2>SUMMER CLASSES</h2>
-          <div className='program-text'>
+          <div className="program-text">
             <Link to="/register">
               <span>
-                Amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus.
-                Cras semper auctor neque vitae tempus.
-                Non consectetur a erat nam at lectus urna duis convallis.
-                Non curabitur gravida arcu ac. Accumsan in nisl nisi scelerisque eu ultrices vitae.
+                Amet volutpat consequat mauris nunc congue nisi vitae suscipit
+                tellus. Cras semper auctor neque vitae tempus. Non consectetur a
+                erat nam at lectus urna duis convallis. Non curabitur gravida
+                arcu ac. Accumsan in nisl nisi scelerisque eu ultrices vitae.
                 Gravida arcu ac tortor dignissim convallis aenean et tortor at.
               </span>
               <p>Register Now!</p>
@@ -60,5 +60,5 @@ export default function Programs() {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React from "react";
+import "../stylesheets/Register.css";
+import registration from "../images/registration.jpg";
 
 export default function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="register-container">
+      <h1>Register</h1>
+      <img src={registration} alt="register" />
+      <form class="row row-cols-lg-auto g-3 align-items-center" action="/">
+        <div class="col-12">
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="input-group">
+            <input
+              type="email"
+              class="form-control"
+              placeholder="Email"
+              required
+            />
+          </div>
+        </div>
+
+        <div class="col-12">
+          <label class="visually-hidden" for="inlineFormSelectPref">
+            Preference
+          </label>
+          <select class="form-select" id="inlineFormSelectPref" required>
+            <option selected>Choose your program</option>
+            <option value="1">On Campus</option>
+            <option value="2">Online</option>
+            <option value="3">Summer Course</option>
+          </select>
+        </div>
+
+        <div class="col-12">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="inlineFormCheck"
+              required
+            />
+            <label class="form-check-label" for="inlineFormCheck">
+              Agree to send my information  
+            </label>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
