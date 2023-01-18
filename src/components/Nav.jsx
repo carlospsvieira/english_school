4e2971099b4d0812e487779caffa1fad2af870d1
Carlos Vieira <carlepsvieira@gmail.com>
2023-01-18 12:09:11 -0300

    hamburger navbar applied

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../stylesheets/Nav.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Nav() {
   return (
@@ -12,6 +13,45 @@ export default function Nav() {
         <Link to="/about">About</Link>
         <Link to="/pricing">Dates & Pricing</Link>
         <Link to="/register">Register</Link>
+      </div>
+      <button
+        className="btn btn-light hamburger"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        <GiHamburgerMenu />
+      </button>
+
+      <div
+        className="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 id="offcanvasRightLabel">English School Links</h5>
+          <button
+            type="button"
+            className="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <ul className="links-list">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Dates & Pricing</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Programs from "../components/Programs";
 import Testimonials from "../components/Testimonials";
+import '../stylesheets/Home.css'
 
 export default function Home() {
   return (
